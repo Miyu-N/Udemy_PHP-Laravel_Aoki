29e59ff9e9f5ca19c641f3aab9ef4ca1de2effbb
Miyu-N <myu.star1217@gmail.com>
2022-02-19 23:23:01 +0900

    5-66 composer実行

---
+++ composertest/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd1e04a4069f8c3a480c4b6242ed29e2fdbe178b3',
+        'name' => 'myust/composertest',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'myust/composertest' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd1e04a4069f8c3a480c4b6242ed29e2fdbe178b3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
